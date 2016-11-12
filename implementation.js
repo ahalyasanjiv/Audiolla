4484ba23f308a86d5606e27967e2871dd8b1f1ae
Ahalya Sanjiv <ahalyasanjiv@gmail.com>
2016-11-12 16:16:02 -0500

    add js file

---
+++ implementation.js
@@ -0,0 +1,7 @@
+function play() {
+	 var audio = document.getElementById("audio");
+	 var notes = ["C", "A", "B", "D", "F", "G", "E",];
+	 var j = Math.floor(Math.random() * notes.length-1);
+	 document.getElementById("audio").src = "notes/" + notes[j]+".wav";
+	 audio.play();
+}
