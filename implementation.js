0b60ca22462316d6b6966f99a63a59cd0adc0abd
Jane Chen <jane.chen@macaulay.cuny.edu>
2016-11-12 19:12:26 -0500

    added randomly ordered notes

--- implementation.js
+++ implementation.js
@@ -1,12 +1,28 @@
 function play() {
-	 var audio = document.getElementById("audio");
-	 var notes = ["C", "A", "B", "D", "F", "G", "E",];
-	 var j = Math.floor(Math.random() * notes.length-1);
-	 document.getElementById("audio").src = "notes/" + notes[j]+".wav";
-	 audio.play();
+	var audio = document.getElementById("audio");
+	var notes = ["A", "B", "C", "D", "E", "F", "G"];
+	var j = Math.floor(Math.random() * notes.length);
+	document.getElementById("audio").src = "notes/" + notes[j]+".wav";
+	audio.play();
 }
 
 function playSound(x) {
 	var audio = document.getElementsByClassName("audio");
 	audio[x].play();
+}
+
+var random; // store the random notes as a global array so we can access this later
+function randomize() {
+	random = []; // reset the array
+	var notes = ["A", "B", "C", "D", "E", "F", "G"];
+
+	var buttonIndex = 0;
+
+	while (notes.length > 0) {
+		var i = Math.floor(Math.random() * notes.length); // notes[i] is the chosen note
+		random.push(notes[i]); // add the randomly chosen note into random
+		document.getElementsByClassName("audio")[buttonIndex].src = "notes/" + notes[i] + ".wav";
+		buttonIndex++;
+		notes.splice(i,1); // delete the chosen note
+	}
 }
